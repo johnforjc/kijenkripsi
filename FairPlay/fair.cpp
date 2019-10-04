373c53af60c950a4d1d35b51fca051a8f58e36b5
Fandi PJ <37364776+fandipj98@users.noreply.github.com>
2019-10-04 21:06:41 +0700

    we2t34

--- FairPlay/fair.cpp
+++ FairPlay/fair.cpp
@@ -1,5 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
+using namespace std;
+
+// class playFair{
+// 	private:
+// 		map<char, int> indexMatrix
+// 		char matrix[5][5];
+// 	public:
+// 	enkripsi();
+// 	dekripsi();
+// };
 
 int main(){
-    
+    // playFair z;
+    // printf("%d\n",z.a);
+	printf("Hello World");
 }
