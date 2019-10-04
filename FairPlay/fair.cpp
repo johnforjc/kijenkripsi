4df99202706b0b1d74d80692338e6f385d5b37dd
ArisatoX <patricksungkharisma27@gmail.com>
2019-10-04 20:43:50 +0700

    extractIndex done

--- FairPlay/fair.cpp
+++ FairPlay/fair.cpp
@@ -1,5 +1,33 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(){
     
+    map <char,int> extractIndex;
+	
+	extractIndex.insert(pair<char, int>(q, 11));
+	extractIndex.insert(pair<char, int>(u, 12));
+	extractIndex.insert(pair<char, int>(a, 13));
+	extractIndex.insert(pair<char, int>(t, 14)); 
+	extractIndex.insert(pair<char, int>(e, 15)); 
+	extractIndex.insert(pair<char, int>(r, 21)); 
+	extractIndex.insert(pair<char, int>(v, 22)); 
+	extractIndex.insert(pair<char, int>(o, 23)); 
+	extractIndex.insert(pair<char, int>(i, 24)); 
+	extractIndex.insert(pair<char, int>(s, 25)); 
+	extractIndex.insert(pair<char, int>(b, 31)); 
+	extractIndex.insert(pair<char, int>(c, 32)); 
+	extractIndex.insert(pair<char, int>(d, 33));
+	extractIndex.insert(pair<char, int>(f, 34)); 
+	extractIndex.insert(pair<char, int>(g, 35));
+	extractIndex.insert(pair<char, int>(h, 41)); 
+	extractIndex.insert(pair<char, int>(k, 42)); 
+	extractIndex.insert(pair<char, int>(l, 43)); 
+	extractIndex.insert(pair<char, int>(m, 44)); 
+	extractIndex.insert(pair<char, int>(n, 45));
+	extractIndex.insert(pair<char, int>(p, 51)); 
+	extractIndex.insert(pair<char, int>(w, 52)); 
+	extractIndex.insert(pair<char, int>(x, 53)); 
+	extractIndex.insert(pair<char, int>(y, 54)); 
+	extractIndex.insert(pair<char, int>(z, 55));       
 }
